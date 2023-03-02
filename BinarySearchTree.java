a66302644f5879aaa17f2687c19e212f4a03c3df
Nevermeant <82971033+TJH2@users.noreply.github.com>
2023-03-02 14:27:25 -0800

    Update BinarySearchTree.java
    - fixed directory() bug - accidentally coded EmployeeInfo instead of employeeInfo
    - fixed addEmployee() bug - accidentally had Jaspreet remove self-referential part of recursive loop so it went on forever

--- BinarySearchTree.java
+++ BinarySearchTree.java
@@ -8,34 +8,47 @@
 
 
 
+
+
 import java.util.*;
 import java.io.*;
 
 public class BinarySearchTree {
 
-    static Employee root;
+    private static Employee root;
+    static Scanner input = new Scanner(System.in);
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in); 
-        String menu="";
+      //Sample Employees
+      root = addEmployee(root, 50, "Jake", "H", "Junior Software Developer");
+      root = addEmployee(root, 40, "Jaspreet", "K", "Cyber Security Specialist");
+      root = addEmployee(root, 60, "David", "R-H", "Product Manager");
+      root = addEmployee(root, 55, " Ben", "D", "Graphic Design Specialist");
 
-        // sample employee
-        Employee root = new Employee(1, "Jake", "Holmes", "Junior Software Developer");
+      System.out.println("This Program Will Allow You To View And Edit The Directory Of Employees At Bronze Hill LLC\n");
 
+      menu();
+
+    } // end of main
+
+    public static void menu() {
+      String menu="";
+      System.out.println("Please Select An Option:");
       while(!menu.equals("s")&&!menu.equals("a")&&!menu.equals("d")&&!menu.equals("q")){
-         System.out.println("This program will show you a list of employees in a company");
-         System.out.println("please selct and option: ");
-         System.out.println("See all employees in search tree(s)");
-         System.out.println("Add employee to search tree(a)");
-         System.out.println("Delete employee from search tree (d)");
-         System.out.println("quit program(q)");
-        if(!menu.equals("s") || !menu.equals("a") || !menu.equals("d") || !menu.equals("q")) {
-            System.out.print("Please select a valid menu option:\t");
-            menu = input.next();
+         System.out.println("See Bronze Hill Employee Directory(s)");
+         System.out.println("Add An Employee To The Bronze Hill Directory(a)");
+         System.out.println("Remove An Employee To The Bronze Hill Directory(d)");
+         System.out.print("Quit Program(q)\t");
+         menu = input.next().toLowerCase();
+        if(!menu.equals("s") && !menu.equals("a") && !menu.equals("d") && !menu.equals("q")) {
+            System.out.println("\nPlease Select A Valid Menu Option:");
         }
       }
 
       if(menu.equals("s")){
+        System.out.println("\nBRONZE HILL EMPLOYEE DIRECTORY:");
         directory(root);
+        System.out.println();
+        menu();
       }
       else if(menu.equals("a")){
        System.out.print("Provide employee ID \t");
@@ -48,9 +61,9 @@ public class BinarySearchTree {
         String lastName = input.next();
           
        System.out.print("Provide occupation:\t");
-        String occupation = input.next(); 
+        String occupation = input.nextLine(); 
 
-         addEmployee(employeeID, firstName, lastName, occupation);           
+         root = addEmployee(root, employeeID, firstName, lastName, occupation);           
           
 
       }
@@ -58,35 +71,30 @@ public class BinarySearchTree {
          System.out.print("delete");
       }
       else if(menu.equals("q")){
-      System.out.println("thank you for your time.");
+      System.out.println("\nThis Program Has Ended.");
          System.exit(0);
       }
-
-    } // end of main
+    }
 
     public static void directory(Employee root) {
-        if (root == null) {
-          System.out.println("There are no employees to view");
-        } 
+        if (root != null) {
         directory(root.left);
-        System.out.println(root.employeeInfo());
+        System.out.println("\n" + root.employeeInfo());
         directory(root.right);
-    }
+        }
+    } // end of directory
     
-     public static void addEmployee(int employeeID, String firstName, String lastName, String occupation){
-
+     public static Employee addEmployee(Employee root, int employeeID, String firstName, String lastName, String occupation) {
 
       if (root == null) {
-         root = new Employee(int employeeID, String firstName, String lastName, String occupation);
+         root = new Employee(employeeID, firstName, lastName, occupation);
          
        } else if (employeeID < root.employeeID) {
-         root.left = addEmployee( employeeID,  firstName,  lastName,  occupation);
-         
-       } else if (employeeID > root.data) {
-         root.right = addEmployee( employeeID,  firstName,  lastName,  occupation);
+         root.left = addEmployee(root.left, employeeID, firstName, lastName, occupation);
+       } else if (employeeID > root.employeeID) {
+         root.right = addEmployee(root.right, employeeID, firstName, lastName, occupation);
       }
- 
-    
+      return root;
     }//end add 
 
     
@@ -113,7 +121,7 @@ class Employee {
         this.right = null;
     }
 
-    public String EmployeeInfo() {
+    public String employeeInfo() {
         return "Employee Name: " + this.firstName + " " + this.lastName + "\nEmployee ID: " + this.employeeID + "\nPosition: " + this.occupation;
     }
 
