72eed02fc39469439a1d17337a594efb586d3980
Nevermeant <82971033+TJH2@users.noreply.github.com>
2023-03-03 16:22:51 -0800

    Update BinarySearchTree.java
    solved remove bug. functional copy for peer review
    have iteration but still need ATL tree for EC

--- BinarySearchTree.java
+++ BinarySearchTree.java
@@ -4,10 +4,7 @@
 // Assignment: Binary Search Tree
 // Reference: Chapter 17
 // Purpose: Use recursion to add/remove employees from a fictional database
-// Extra Credit: Iterative Employee Search Function IterativeSearch()
-
-
-
+// Extra Credit: Iterative Employee Search Function IterativeSearch() line 128-147
 
 
 import java.util.*;
@@ -16,7 +13,7 @@ public class BinarySearchTree {
 
    static Scanner input = new Scanner(System.in);
    private static Employee root; // root node
-   private static String company = "Arctic Gun Steel Energy Drink LLC";
+   private static String company = "Arctic Gun Metal Energy Drink LLC";
     
     
     public static void main(String[] args) {
@@ -33,22 +30,21 @@ public class BinarySearchTree {
     } // end of main
 
     public static void menu() {
-      String menu="";
-      System.out.println("Please Select An Option:");
-      while(!menu.equals("s")&&!menu.equals("a")&&!menu.equals("l")&&!menu.equals("q")){
-         System.out.println("View " + company + " Employee Directory(s)");
-         System.out.println("Add An Employee To The " + company + " Directory(a)");
-         System.out.println("Edit Specific Employee Information From The " + company + " Directory(l)");
-         System.out.print("Quit Program(q)\t");
-         menu = input.next().toLowerCase();
-        if(!menu.equals("s") && !menu.equals("a") && !menu.equals("l") && !menu.equals("q")) {
-            System.out.println("\nPlease Select A Valid Menu Option:");
-        }
+      int menu = 0;
+      while(menu < 1 || menu > 4){
+         System.out.println("MAIN MENU:");
+         System.out.println("1. View " + company + " Employee Directory");
+         System.out.println("2. Add An Employee To The " + company + " Directory");
+         System.out.println("3. Update Specific Employee Information From The " + company + " Directory");
+         System.out.println("4. Quit Program");
+         System.out.print("\nPlease Select A Valid Menu Option Between 1 And 4:\t");
+         menu = input.nextInt();
+         System.out.println();
       }
 
-      if(menu.equals("s")){
+      if(menu == 1){
         int choice = 0;
-        System.out.print("Would You Like To View An In-Order(1), Pre-Order(2) or Post-Order(3) Display?");
+        System.out.print("How Would You Like To View The Display?:\n1. In-Order\n2. Pre-Order\n3. Post-Order\t");
         choice = input.nextInt();
         System.out.println("\n" + company.toUpperCase() + " EMPLOYEE DIRECTORY:");
         if(choice == 1) {
@@ -63,7 +59,7 @@ public class BinarySearchTree {
         }
         menu();
       }
-      else if(menu.equals("a")){
+      else if(menu == 2){
        System.out.print("\nProvide employee ID \t");
          int employeeID = input.nextInt();
      
@@ -73,20 +69,20 @@ public class BinarySearchTree {
        System.out.print("Provide Last Name:\t");
         String lastName = input.next();
           
-       System.out.print("Provide occupation:\t");
-        String occupation = input.nextLine(); // extra line to change from next to nextLine
-        occupation = input.nextLine();  
+       System.out.print("Provide Position:\t");
+        String position = input.nextLine(); // extra line to change from next to nextLine
+        position = input.nextLine();  
 
-         root = addEmployee(root, employeeID, firstName, lastName, occupation);
+         root = addEmployee(root, employeeID, firstName, lastName, position);
          
          System.out.println("\n" + firstName + " " + lastName + " Has Been Successfully Added To The Directory\n");
          
          menu();           
       }
-      else if(menu.equals("l")){
+      else if(menu == 3){
          employeeSearch();
       }
-      else if(menu.equals("q")){
+      else if(menu == 4){
       System.out.println("\nThis Program Has Ended.");
          System.exit(0);
       }
@@ -98,27 +94,27 @@ public class BinarySearchTree {
    
          Employee temp = iterativeSearch();
          if(temp == null) {
-            System.out.print("\nIt Doesn't Look Like That Employee ID Matches Any Existing Employees. Would You Like To Return To The Menu(1) Or Try Again(2)?");
+            System.out.print("\nIt Doesn't Look Like That Employee ID Matches Any Existing Employees. Would You Like To:\n1. Return To The Menu\n2. Try Again \t");
             choice = input.nextInt();
             if(choice == 1) { menu(); }
             else { employeeSearch(); }
          } // end of if
          else {
             System.out.println("\n" + temp.employeeInfo() + "\n");
-            System.out.print("Would You Like To Remove Employee From The Directory(1), Edit Employee Information(2), Or Return To The Main Menu(3)?\t");
+            System.out.print("Would You Like To:\n1. Remove Employee From The Directory\n2. Edit Employee Information\n3. Return To The Main Menu\t");
             choice = input.nextInt();
             System.out.println();
             if(choice == 1){
-               System.out.print("Are You Sure You Want To Remove " + temp.firstName + " " + temp.lastName + " From The Directory? 1 = No, 2 = Yes");
+               System.out.print("Are You Sure You Want To Remove " + temp.firstName + " " + temp.lastName + " From The Directory?:\n1. YES\n2. NO\t");
                choice = input.nextInt();
                if(choice == 1) {
-                     System.out.println("\nOK, " + temp.firstName + " " + temp.lastName + " Was Not Removed.\n");
-                     menu(); 
-               } else {
-                        //remove(temp.employeeID);
+                        root = remove(root, temp.employeeID);
                         System.out.println("Employee Has Been Successfully Removed!");
                         menu();
-                }
+                } else {
+                     System.out.println("\nOK, " + temp.firstName + " " + temp.lastName + " Was Not Removed.\n");
+                     menu(); 
+               }
             }
             else if(choice == 2) {
                editEmployee(temp);
@@ -131,16 +127,20 @@ public class BinarySearchTree {
     public static Employee iterativeSearch() {
       System.out.print("\nPlease Provide The ID Of The Employee You're Searching For\t");
          int employeeID = input.nextInt();
+         Employee temp = root;
     
-      while(root != null) {
+      while(temp != null) {
         // pass right subtree as new tree
-        if (employeeID > root.employeeID)
-            root = root.right;
+        if (employeeID > temp.employeeID) {
+            temp = temp.right; 
+        }
         // pass left subtree as new tree
-        else if (employeeID < root.employeeID)
-            root = root.left;
-        else
-            return root; // if the ID is found
+        else if (employeeID < temp.employeeID) {
+            temp = temp.left;
+        }
+        else {
+            return temp; // if the ID is found
+        }
       }
       return null;
     } // end of IS
@@ -149,9 +149,9 @@ public class BinarySearchTree {
     // Method to edit Employee
     public static void editEmployee(Employee temp) {
     String edit;
-     int finished = 1;
-     while(finished == 1){
-        System.out.print("What Information Would You Like To Update, First Name(1), Last Name(2) Or Position(3)?\t");
+     int finished = 2;
+     while(finished == 2){
+        System.out.print("What Information Would You Like To Update:\n1. First Name\n2. Last Name\n3. Position\t");
         int choice = input.nextInt();
         if(choice == 1) {
          System.out.print("Please Provide A New First Name:\t");
@@ -171,7 +171,7 @@ public class BinarySearchTree {
         
         System.out.println("\nEmployee Information Has Been Successfully Updated!");
         System.out.println("\n" + temp.employeeInfo());
-        System.out.print("\n Have You Completed Your Updates For This Employee? 1 = No, 2 = Yes");
+        System.out.print("\n Have You Completed Your Updates For This Employee?:\n1. Yes\n2. NO\t");
         finished = input.nextInt();
      }
       System.out.println();
@@ -218,6 +218,34 @@ public class BinarySearchTree {
       }
       return root;
     }//end add 
+    
+    
+    
+    public static Employee remove(Employee node, int employeeID) {
+    if(node == null) { return null; }
+    
+    if(employeeID < node.employeeID) {
+    node.left = remove(node.left, employeeID);
+    } else if(employeeID > node.employeeID) {
+    node.right = remove(node.right, employeeID);
+    } else {
+            if(node.left == null && node.right != null) { // right open
+               return node.right; 
+            } else if(node.right == null && node.left != null) { // left open
+               return node.left;   
+            } else if (node.right == null && node.left == null) { return null ; } // neither      
+             else {
+               Employee nodeRight = node.right;
+               while(nodeRight.left != null) { nodeRight = nodeRight.left; } // both
+               node.firstName = nodeRight.firstName;
+               node.lastName = nodeRight.lastName;
+               node.position = nodeRight.position;
+               node.employeeID = nodeRight.employeeID;
+               node.right = remove(node.right, nodeRight.employeeID);
+      }
+      } // end of else
+    return node;
+    }
     
 } // end of lab6 class
 
